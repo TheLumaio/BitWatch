4fcf3a6350c8a2d1550bc772bbc499021682c436
Patrick Keckler <patrick@kecklers.com>
2017-09-02 17:25:08 -0400

    Who uses spaces? ew.

--- BitWatch/BitWatch.cs
+++ BitWatch/BitWatch.cs
@@ -1,108 +1,109 @@
-﻿
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Text;
 using System.Threading;
 
 namespace BitWatch
 {
-    public class BitWatch
-    {
-        private static Bittrex bit;
-        private static string key = "7a8f51b3a2fa44afb8993c6dd0b50d08";    // dont hack plz
-        private static string secret = "184cc8ccaba04a0f8c73a055d0353ee2"; // i dont want my bitz stolen thx
-        private static double btcvalue = 0;
-        
-        public static void Main(string[] args)
-        {
-            Console.SetWindowSize(100, 25);
-            
-            bit = new Bittrex(key, secret);
-            
-            bit.GetOpenOrders();
-            var balances = bit.GetBalances();
+	public class BitWatch
+	{
+		private static Bittrex bit;
+		private static string key = "7a8f51b3a2fa44afb8993c6dd0b50d08"; // dont hack plz
+		private static string secret = "184cc8ccaba04a0f8c73a055d0353ee2"; // i dont want my bitz stolen thx
+		private static double btcvalue = 0;
 
-            Console.CursorVisible = false;
-            
-            Console.Clear();
-            Console.WriteLine("COIN : History                :         Balance :       BTC Value :  USD Value :    USD P/C");
-            Console.WriteLine("-------------------------------------------------------------------------------------------");
-            Console.SetCursorPosition(0, balances.result.Count+4);
-            Console.WriteLine("Press <Q> to stop.");
-            
-            BackgroundWorker worker = new BackgroundWorker();
-            worker.DoWork += delegate
-            {
-                while (!Console.KeyAvailable)
-                {
-                    btcvalue = bit.GetBtcValue().result.Last;
-                    balances = bit.GetBalances();
-                    PrintBalances(balances);
-                    Thread.Sleep(5000);
-                }
-            };
-            worker.RunWorkerAsync();
+		public static void Main(string[] args)
+		{
+			Console.SetWindowSize(100, 25);
 
-            while (Console.ReadKey(true).Key != ConsoleKey.Q);
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.CursorVisible = true;
-        }
+			bit = new Bittrex(key, secret);
 
-        private static void PrintBalances(Balances balances)
-        {
-            Console.SetCursorPosition(0, 2);
-            double totalbits = 0;
-            foreach (var b in balances.result)
-            {
-                if (b.Currency == "BTC")
-                {
-                    totalbits += b.Available;
-                    var busdval = Math.Round(btcvalue * b.Available, 2);
-                    Console.Write("BTC  : ");
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write("----------------------");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.WriteLine(" : {0,15:F10} :             N/A : ${2,9:F2} : ${3,9:F2}", b.Available, btcvalue*b.Available, busdval, btcvalue);
-                    continue;
-                };
+			bit.GetOpenOrders();
+			var balances = bit.GetBalances();
 
-                if (b.Balance == 0)
-                    continue;
-                
-                Console.Write($"{b.Currency, -5}: ");
-                var trades = bit.GetTrades($"BTC-{b.Currency}");
-                totalbits += trades.result[0].Price * b.Balance;
+			Console.CursorVisible = false;
 
-                var last = 0.0;
-                for (var i = 0; i < 22; i++)
-                {
-                    last = trades.result[i + 1].Price;
-                    var t = trades.result[i];
-                    if (t.Price > last)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write("↑");
-                    }
-                    else if (t.Price < last)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("↓");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.Write("-");
-                    }
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                }
-                var tradebal = trades.result[0].Price * b.Balance;
-                var usdval = Math.Round(btcvalue * tradebal, 2);
-                var usdpc = Math.Round(btcvalue * trades.result[0].Price, 2);
-                Console.WriteLine(" : {0,15:F10} : {1,15:F10} : ${2,9:F2} : ${3,9:F2}", b.Available, tradebal, usdval, usdpc);
-            }
-            Console.WriteLine("\nTotal BTC value: {0}", totalbits);
-            Console.WriteLine("Total USD value: ${0:F2}", Math.Round(btcvalue*totalbits, 2));
-        }
-    }
+			Console.Clear();
+			Console.WriteLine("COIN : History                :         Balance :       BTC Value :  USD Value :    USD P/C");
+			Console.WriteLine("-------------------------------------------------------------------------------------------");
+			Console.SetCursorPosition(0, balances.result.Count + 4);
+			Console.WriteLine("Press <Q> to stop.");
+
+			BackgroundWorker worker = new BackgroundWorker();
+			worker.DoWork += delegate
+			{
+				while (!Console.KeyAvailable)
+				{
+					btcvalue = bit.GetBtcValue().result.Last;
+					balances = bit.GetBalances();
+					PrintBalances(balances);
+					Thread.Sleep(5000);
+				}
+			};
+			worker.RunWorkerAsync();
+
+			while (Console.ReadKey(true).Key != ConsoleKey.Q) ;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Gray;
+			Console.CursorVisible = true;
+		}
+
+		private static void PrintBalances(Balances balances)
+		{
+			Console.SetCursorPosition(0, 2);
+			double totalbits = 0;
+			foreach (var b in balances.result)
+			{
+				if (b.Currency == "BTC")
+				{
+					totalbits += b.Available;
+					var busdval = Math.Round(btcvalue * b.Available, 2);
+					Console.WriteLine("BTC  : ");
+					Console.ForegroundColor = ConsoleColor.DarkGray;
+					Console.Write("----------------------");
+					Console.ForegroundColor = ConsoleColor.Gray;
+					Console.WriteLine(" : {0,15:F10} :             N/A : ${2,9:F2} : ${3,9:F2}", b.Available, btcvalue * b.Available,
+						busdval, btcvalue);
+					continue;
+				}
+				;
+
+				if (b.Balance == 0)
+					continue;
+
+				Console.Write($"{b.Currency,-5}: ");
+				var trades = bit.GetTrades($"BTC-{b.Currency}");
+				totalbits += trades.result[0].Price * b.Balance;
+
+				var last = 0.0;
+				for (var i = 0; i < 22; i++)
+				{
+					last = trades.result[i + 1].Price;
+					var t = trades.result[i];
+					if (t.Price > last)
+					{
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.Write("↑");
+					}
+					else if (t.Price < last)
+					{
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.Write("↓");
+					}
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.DarkGray;
+						Console.Write("-");
+					}
+					Console.ForegroundColor = ConsoleColor.Gray;
+				}
+				var tradebal = trades.result[0].Price * b.Balance;
+				var usdval = Math.Round(btcvalue * tradebal, 2);
+				var usdpc = Math.Round(btcvalue * trades.result[0].Price, 2);
+				Console.WriteLine(" : {0,15:F10} : {1,15:F10} : ${2,9:F2} : ${3,9:F2}", b.Available, tradebal, usdval, usdpc);
+			}
+			Console.WriteLine("\nTotal BTC value: {0}", totalbits);
+			Console.WriteLine("Total USD value: ${0:F2}", Math.Round(btcvalue * totalbits, 2));
+		}
+	}
 }
--- BitWatch/Bittrex.cs
+++ BitWatch/Bittrex.cs
@@ -9,160 +9,155 @@ using Newtonsoft.Json;
 
 namespace BitWatch
 {
-    /* Balances */
-    public class Coin
-    {
-        public string Currency { get; set; }
-        public double Balance { get; set; }
-        public double Available { get; set; }
-        public double Pending { get; set; }
-        public string CryptoAddress { get; set; }
-        public bool Requested { get; set; }
-        public object Uuid { get; set; }
-    }
+	/* Balances */
+	public class Coin
+	{
+		public string Currency { get; set; }
+		public double Balance { get; set; }
+		public double Available { get; set; }
+		public double Pending { get; set; }
+		public string CryptoAddress { get; set; }
+		public bool Requested { get; set; }
+		public object Uuid { get; set; }
+	}
 
-    public class Balances
-    {
-        public bool success { get; set; }
-        public string message { get; set; }
-        public List<Coin> result { get; set; }
-    }
-    
-    /* Trades */
-    public class Trade
-    {
-        public int Id { get; set; }
-        public string TimeStamp { get; set; }
-        public double Quantity { get; set; }
-        public double Price { get; set; }
-        public double Total { get; set; }
-        public string FillType { get; set; }
-        public string OrderType { get; set; }
-    }
+	public class Balances
+	{
+		public bool success { get; set; }
+		public string message { get; set; }
+		public List<Coin> result { get; set; }
+	}
 
-    public class Trades
-    {
-        public bool success { get; set; }
-        public string message { get; set; }
-        public List<Trade> result { get; set; }
-    }
-    
-    /* BTC value */
-    public class Value
-    {
-        public double Bid { get; set; }
-        public double Ask { get; set; }
-        public double Last { get; set; }
-    }
+	/* Trades */
+	public class Trade
+	{
+		public int Id { get; set; }
+		public string TimeStamp { get; set; }
+		public double Quantity { get; set; }
+		public double Price { get; set; }
+		public double Total { get; set; }
+		public string FillType { get; set; }
+		public string OrderType { get; set; }
+	}
 
-    public class BTCValue
-    {
-        public bool success { get; set; }
-        public string message { get; set; }
-        public Value result { get; set; }
-    }
-    
-    /* Bittrex */
-    public class Bittrex
-    {
-        
-        private static string apisign = "";
-        private static string url = "";
-        private static long nonce = 0;
-        private static string key = "";
-        private static string secret = "";
-        private static readonly HttpClient client = new HttpClient();
-        
-        public Bittrex(string _key, string _secret)
-        {
-            nonce = System.DateTime.Now.Ticks;
-            Console.WriteLine("Nonce/Time: {0}", nonce);
+	public class Trades
+	{
+		public bool success { get; set; }
+		public string message { get; set; }
+		public List<Trade> result { get; set; }
+	}
 
-            key = _key;
-            secret = _secret;
+	/* BTC value */
+	public class Value
+	{
+		public double Bid { get; set; }
+		public double Ask { get; set; }
+		public double Last { get; set; }
+	}
 
-        }
-        
-        private static string HashHmac(string message, string secret)
-        {
-            Encoding encoding = Encoding.UTF8;
-            using (HMACSHA512 hmac = new HMACSHA512(encoding.GetBytes(secret)))
-            {
-                var msg = encoding.GetBytes(message);
-                var hash = hmac.ComputeHash(msg);
-                return BitConverter.ToString(hash).ToLower().Replace("-", string.Empty);
-            }
-        }
+	public class BTCValue
+	{
+		public bool success { get; set; }
+		public string message { get; set; }
+		public Value result { get; set; }
+	}
 
-        public void GetOpenOrders()
-        {
-            
-            url = string.Format("https://bittrex.com/api/v1.1/market/getopenorders?apikey={0}&nonce={1}", key, nonce);
-            apisign = HashHmac(url, secret);
-            
-            var request = new HttpRequestMessage()
-            {
-                RequestUri = new Uri(url),
-                Method = HttpMethod.Get
-            };
-            request.Headers.Add("apisign", apisign);
-            
-            HttpResponseMessage response = client.SendAsync(request).Result;
-            string message = response.Content.ReadAsStringAsync().Result;
-        }
+	/* Bittrex */
+	public class Bittrex
+	{
+		private static string apisign = "";
+		private static string url = "";
+		private static long nonce = 0;
+		private static string key = "";
+		private static string secret = "";
+		private static readonly HttpClient client = new HttpClient();
 
-        public Balances GetBalances()
-        {
-            url = string.Format("https://bittrex.com/api/v1.1/account/getbalances?apikey={0}&nonce={1}", key, nonce);
-            apisign = HashHmac(url, secret);
+		public Bittrex(string _key, string _secret)
+		{
+			nonce = System.DateTime.Now.Ticks;
+			Console.WriteLine("Nonce/Time: {0}", nonce);
 
-            var request = new HttpRequestMessage()
-            {
-                RequestUri = new Uri(url),
-                Method = HttpMethod.Get
-            };
-            request.Headers.Add("apisign", apisign);
+			key = _key;
+			secret = _secret;
+		}
 
-            HttpResponseMessage response = client.SendAsync(request).Result;
-            string message = response.Content.ReadAsStringAsync().Result;
-            Balances balances = JsonConvert.DeserializeObject<Balances>(message);
-            
-            return balances;
-        }
+		private static string HashHmac(string message, string secret)
+		{
+			Encoding encoding = Encoding.UTF8;
+			using (HMACSHA512 hmac = new HMACSHA512(encoding.GetBytes(secret)))
+			{
+				var msg = encoding.GetBytes(message);
+				var hash = hmac.ComputeHash(msg);
+				return BitConverter.ToString(hash).ToLower().Replace("-", string.Empty);
+			}
+		}
 
-        public Trades GetTrades(string market)
-        {
-            url = $"https://bittrex.com/api/v1.1/public/getmarkethistory?market={market}";
-            
-            var request = new HttpRequestMessage()
-            {
-                RequestUri = new Uri(url),
-                Method = HttpMethod.Get
-            };
+		public void GetOpenOrders()
+		{
+			url = string.Format("https://bittrex.com/api/v1.1/market/getopenorders?apikey={0}&nonce={1}", key, nonce);
+			apisign = HashHmac(url, secret);
 
-            HttpResponseMessage response = client.SendAsync(request).Result;
-            string message = response.Content.ReadAsStringAsync().Result;
-            Trades trades = JsonConvert.DeserializeObject<Trades>(message);
-            
-            return trades;
-        }
+			var request = new HttpRequestMessage()
+			{
+				RequestUri = new Uri(url),
+				Method = HttpMethod.Get
+			};
+			request.Headers.Add("apisign", apisign);
 
-        public BTCValue GetBtcValue()
-        {
-            url = $"https://bittrex.com/api/v1.1/public/getticker?market=usdt-btc";
-            var request = new HttpRequestMessage()
-            {
-                RequestUri = new Uri(url),
-                Method = HttpMethod.Get
-            };
+			HttpResponseMessage response = client.SendAsync(request).Result;
+			string message = response.Content.ReadAsStringAsync().Result;
+		}
 
-            HttpResponseMessage response = client.SendAsync(request).Result;
-            string message = response.Content.ReadAsStringAsync().Result;
-            BTCValue value = JsonConvert.DeserializeObject<BTCValue>(message);
+		public Balances GetBalances()
+		{
+			url = string.Format("https://bittrex.com/api/v1.1/account/getbalances?apikey={0}&nonce={1}", key, nonce);
+			apisign = HashHmac(url, secret);
 
-            return value;
-        }
-    }
-    
-    
+			var request = new HttpRequestMessage()
+			{
+				RequestUri = new Uri(url),
+				Method = HttpMethod.Get
+			};
+			request.Headers.Add("apisign", apisign);
+
+			HttpResponseMessage response = client.SendAsync(request).Result;
+			string message = response.Content.ReadAsStringAsync().Result;
+			Balances balances = JsonConvert.DeserializeObject<Balances>(message);
+
+			return balances;
+		}
+
+		public Trades GetTrades(string market)
+		{
+			url = $"https://bittrex.com/api/v1.1/public/getmarkethistory?market={market}";
+
+			var request = new HttpRequestMessage()
+			{
+				RequestUri = new Uri(url),
+				Method = HttpMethod.Get
+			};
+
+			HttpResponseMessage response = client.SendAsync(request).Result;
+			string message = response.Content.ReadAsStringAsync().Result;
+			Trades trades = JsonConvert.DeserializeObject<Trades>(message);
+
+			return trades;
+		}
+
+		public BTCValue GetBtcValue()
+		{
+			url = $"https://bittrex.com/api/v1.1/public/getticker?market=usdt-btc";
+			var request = new HttpRequestMessage()
+			{
+				RequestUri = new Uri(url),
+				Method = HttpMethod.Get
+			};
+
+			HttpResponseMessage response = client.SendAsync(request).Result;
+			string message = response.Content.ReadAsStringAsync().Result;
+			BTCValue value = JsonConvert.DeserializeObject<BTCValue>(message);
+
+			return value;
+		}
+	}
 }
