e85994894c5f53ec7cd17c3be7d1b0c9d5b9f7a8
Patrick Keckler <patrick@kecklers.com>
2017-09-02 18:46:11 -0400

    Refactor and history changes.

--- BitWatch/BitWatch.cs
+++ BitWatch/BitWatch.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Text;
 using System.Threading;
 
 namespace BitWatch
@@ -8,8 +7,8 @@ namespace BitWatch
 	public class BitWatch
 	{
 		private static Bittrex bit;
-		private static string key = "7a8f51b3a2fa44afb8993c6dd0b50d08"; // dont hack plz
-		private static string secret = "184cc8ccaba04a0f8c73a055d0353ee2"; // i dont want my bitz stolen thx
+		private static readonly string key = "7a8f51b3a2fa44afb8993c6dd0b50d08"; // dont hack plz
+		private static readonly string secret = "184cc8ccaba04a0f8c73a055d0353ee2"; // i dont want my bitz stolen thx
 		private static double btcvalue = 0;
 
 		public static void Main(string[] args)
@@ -29,7 +28,7 @@ namespace BitWatch
 			Console.SetCursorPosition(0, balances.result.Count + 4);
 			Console.WriteLine("Press <Q> to stop.");
 
-			BackgroundWorker worker = new BackgroundWorker();
+			var worker = new BackgroundWorker();
 			worker.DoWork += delegate
 			{
 				while (!Console.KeyAvailable)
@@ -58,42 +57,35 @@ namespace BitWatch
 				{
 					totalbits += b.Available;
 					var busdval = Math.Round(btcvalue * b.Available, 2);
-					Console.WriteLine("BTC  : ");
+					Console.Write("BTC  : ");
 					Console.ForegroundColor = ConsoleColor.DarkGray;
 					Console.Write("----------------------");
 					Console.ForegroundColor = ConsoleColor.Gray;
-					Console.WriteLine(" : {0,15:F10} :             N/A : ${2,9:F2} : ${3,9:F2}", b.Available, btcvalue * b.Available,
-						busdval, btcvalue);
+					Console.WriteLine(" : {0,15:F10} :             N/A : ${1,9:F2} : ${2,9:F2}", b.Available, busdval, btcvalue);
 					continue;
 				}
-				;
+				
 
-				if (b.Balance == 0)
+				if ((int)b.Balance == 0)
 					continue;
 
 				Console.Write($"{b.Currency,-5}: ");
 				var trades = bit.GetTrades($"BTC-{b.Currency}");
 				totalbits += trades.result[0].Price * b.Balance;
 
-				var last = 0.0;
 				for (var i = 0; i < 22; i++)
 				{
-					last = trades.result[i + 1].Price;
+					var last = trades.result[i + 1].Price;
 					var t = trades.result[i];
-					if (t.Price > last)
+					if (t.OrderType.Contains("BUY"))
 					{
 						Console.ForegroundColor = ConsoleColor.Green;
 						Console.Write("↑");
 					}
-					else if (t.Price < last)
+					else if (t.OrderType.Contains("SELL"))
 					{
 						Console.ForegroundColor = ConsoleColor.Red;
 						Console.Write("↓");
-					}
-					else
-					{
-						Console.ForegroundColor = ConsoleColor.DarkGray;
-						Console.Write("-");
 					}
 					Console.ForegroundColor = ConsoleColor.Gray;
 				}
